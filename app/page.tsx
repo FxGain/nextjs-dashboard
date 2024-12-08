95621ee4e6e41d29e2301b81ad00d193acd921fa
Fundrise <153876056+FxGain@users.noreply.github.com>
2024-12-08 02:28:21 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,11 @@ export default function Page() {
             
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to Fintual FX.</strong> Invest in your tomorrow today!e{' '}
+            <a href="get started " className="text-blue-500">
+              now,
             </a>
-            , brought to you by Vercel.
+            , already have an account?.
             </p>
           <Link
             href="/login"
